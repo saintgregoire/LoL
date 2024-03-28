ab40af7006931a2b58ef4b63dcb259cf2fe40dc7
VOZNICHKA Maksym <maksym.voznicka@gmail.com>
2024-03-28 17:23:35 +0100

    js modale started

--- js/style.js
+++ js/style.js
@@ -9,7 +9,27 @@ window.addEventListener('DOMContentLoaded', function () {
       navbar.classList.toggle('burger-display')
     }
 
+    // **MODALE ROLE
+  let ashe = document.querySelector('.ashe');
+  let asheModale = document.querySelector('.ashe_big');
 
+  let caitlyn = document.querySelector('.caitlyn');
+  let caitlynModale = document.querySelector('.caitlyn_big');
+
+  let lucian = document.querySelector('.lucian');
+  let lucianModale = document.querySelector('.lucian_big');
+
+  let lulu = document.querySelector('.lulu');
+  let luluModale = document.querySelector('.lulu_big');
+
+  let nautilus = document.querySelector('.nautilus');
+  let nautilusModale = document.querySelector('.nautilus_big');
+
+  let rakan = document.querySelector('.rakan');
+  let rakanModale = document.querySelector('.rakan_big');
+
+  let rammus = document.querySelector('.rammus');
+  let rammusModale = document.querySelector('.rammus_big');
 
   })
   
