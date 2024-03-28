8745d689947f41aa30c55ec268e92da6d9fbf1ed
Kilian Janus <januskilian@gmail.com>
2024-03-28 16:35:45 +0000

    Last commit Media Jeudi

--- js/style.js
+++ js/style.js
@@ -1,12 +1,19 @@
 window.addEventListener('DOMContentLoaded', function () {
     //header burger menu
-    let navbar = document.getElementById('navbar')
-    let burgerIcon = document.getElementById('burger-icon')
-    burgerIcon.addEventListener('click', toggleBurgerMenu)
+    let navbar = document.getElementById('navbar');
+    let burgerIcon = document.getElementById('burger-icon');
+    burgerIcon.addEventListener('click', toggleBurgerMenu);
   
     function toggleBurgerMenu() {
-      console.log('enter toggle')
-      navbar.classList.toggle('burger-display')
+      console.log('enter toggle');
+      navbar.classList.toggle('burger-display');
     }
-  })
+    
+    /*let slider = document.getElementById('.slider');
+    slider.addEventListener('click', function(event) {
+      let body = document.querySelector('.all_elements');
+      body.style.fontFamily = "sans-serif";
+    })*/
+    
+  });
   
